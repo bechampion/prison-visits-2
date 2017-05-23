233a2f375b601fda1a6312fb540c535fd597bfa0
Alan Kennedy <alankennedy100@gmail.com>
2017-05-23 15:56:05 +0100

    Use nomis booker in accept processor

--- app/services/booking_responder/accept.rb
+++ app/services/booking_responder/accept.rb
@@ -5,8 +5,18 @@ class BookingResponder
         visit.rejection = nil
         visit.accept!
 
-        BookingResponse.succesful
+        if visit.book_to_nomis_opt_out?
+          BookingResponse.succesful
+        else
+          booker.book
+        end
       end
+    end
+
+  private
+
+    def booker
+      @booker ||= Nomis::Booker.new(visit)
     end
   end
 end
--- spec/services/booking_responder/accept_spec.rb
+++ spec/services/booking_responder/accept_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe BookingResponder::Accept do
       uv.not_on_list = true
       params[:visitors_attributes][params[:visitors_attributes].size] = uv.attributes.slice('id', 'banned', 'not_on_list')
     end
-    banned_visitors.map do |bv|
+    banned_visitors.each do |bv|
       bv.banned = true
       params[:visitors_attributes][params[:visitors_attributes].size] = bv.attributes.slice('id', 'banned', 'not_on_list')
     end
@@ -25,13 +25,8 @@ RSpec.describe BookingResponder::Accept do
 
   subject { described_class.new(staff_response) }
 
-  describe 'with a message' do
-    before do
-      visit.assign_attributes(params)
-      expect(staff_response).to be_valid
-    end
-
-    it 'process the request' do
+  shared_examples_for 'process the request' do
+    it 'updates the visit' do
       subject.process_request
       expect(visit.reference_no).to eq(params[:reference_no])
       expect(visit.slot_granted.to_s).to eq(params[:slot_granted])
@@ -39,5 +34,35 @@ RSpec.describe BookingResponder::Accept do
       expect(visit.banned_visitors).to eq(banned_visitors)
       expect(visit.unlisted_visitors).to eq(unlisted_visitors)
     end
+  end
+
+  before do
+    visit.assign_attributes(params)
+    expect(staff_response).to be_valid
+  end
+
+  describe 'with not booking to nomis' do
+    before do
+      visit.book_to_nomis_opt_out = true
+    end
+
+    it_behaves_like 'process the request'
+  end
+
+  describe 'with book to nomis' do
+    let(:booker) { double(Nomis::Booker) }
+
+    before do
+      visit.book_to_nomis_opt_out = false
+      mock_service_with(Nomis::Booker, booker)
+      allow(booker).to receive(:book)
+    end
+
+    it 'attempts to book the visit through the booker' do
+      expect(booker).to receive(:book).and_return(BookingResponse.succesful)
+      subject.process_request
+    end
+
+    it_behaves_like 'process the request'
   end
 end
