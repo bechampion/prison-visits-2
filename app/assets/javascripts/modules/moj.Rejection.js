1798b81398fd4bfe23be146acaed94d03a2e3cca
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-06-06 15:27:28 +0100

    Show/hide the Nomis opt out based on rejection

--- app/assets/javascripts/modules/moj.Rejection.js
+++ app/assets/javascripts/modules/moj.Rejection.js
@@ -4,6 +4,8 @@
   moj.Modules.Rejection = {
     el: '.js-Rejection',
 
+    $booktonomis: $('#nomis-opt-out'),
+
     selected: [],
 
     init: function () {
@@ -62,14 +64,28 @@
       if(this.selected.length > 0){
         $conditionalEl.show();
         $conditionalEl.attr('aria-expanded', 'true').attr('aria-hidden', 'false');
+        this.hideBookToNomis();
       } else {
         $conditionalEl.hide();
         $conditionalEl.attr('aria-expanded', 'false').attr('aria-hidden', 'true');
+        this.showBookToNomis();
       }
     },
 
     conditionals: function(string) {
       return $(string ? '#' + string.split(',').join(',#') : null);
+    },
+
+    hideBookToNomis: function(){
+      this.$booktonomis.hide().attr('aria-expanded', 'false').attr('aria-hidden', 'true');
+      $('input[name="visit[book_to_nomis_opt_out]"]').val(1);
+      $('#visit_book_to_nomis_opt_out').prop('checked', true);
+    },
+
+    showBookToNomis: function(){
+      this.$booktonomis.show().attr('aria-expanded', 'true').attr('aria-hidden', 'false');
+      $('input[name="visit[book_to_nomis_opt_out]"]').val(0);
+      $('#visit_book_to_nomis_opt_out').prop('checked', false);
     }
   };
 }());
